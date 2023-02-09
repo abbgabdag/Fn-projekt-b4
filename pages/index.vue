d59c43ce6ed2601d775e2dbb0f6ec77b12b26b76
abbgabdag <abbgabdag@gmail.se>
2023-02-09 13:12:41 +0100

    first commit to main

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>
+            hej hur mår du
+
+        </p>
+    </div>
+</template>
+
+<script setup>
+ 
+</script>
+
+<style >
+ p {
+    background-color: aqua;
+ }
+</style>
